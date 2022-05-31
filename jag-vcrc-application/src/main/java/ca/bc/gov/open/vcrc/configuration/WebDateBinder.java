d8d9ba0ab54986e166160703d90fd35fdb88ed11
ekwan <224302@nttdata.com>
2022-05-31 10:37:33 -0700

    add date binder

---
+++ jag-vcrc-application/src/main/java/ca/bc/gov/open/vcrc/configuration/WebDateBinder.java
@@ -0,0 +1,20 @@
+package ca.bc.gov.open.vcrc.configuration;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.support.WebBindingInitializer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@ControllerAdvice
+public class WebDateBinder implements WebBindingInitializer {
+    @Override
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        binder.registerCustomEditor(       Date.class,
+                new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true, 10));
+    }
+}
